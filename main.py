340f193ce09400440079fbac9675bf89b284d528
Amir Moeini <moeiniamir@outlook.com>
2023-07-15 05:38:52 +0330

    use config for constants
    to make the program more maintainable when the configuration changes we add a dict config in main.py.

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import time
 import hetzner_utils
 import check_utils
 
+CONFIG = {
+    'domain': 'example.com',
+    'user': 'root',
+    'timeout': 5,
+    'interval': 60 * 3
+}
+
 
 def maintain():
     while True:
         try:
-            check_utils.check_ip('example.com', 'root', 5)
+            check_utils.check_ip(CONFIG['domain'], CONFIG['user'], CONFIG['timeout'])
         except Exception as e:
             hetzner_utils.change_ip()
-        time.sleep(60*3)
+        time.sleep(CONFIG['interval'])
 
 
 maintain()
