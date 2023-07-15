257fa7f8c8acd98582b19a3ac814e7806d50e325
Amir Moeini <moeiniamir@outlook.com>
2023-07-15 05:55:20 +0330

    add retry mechanism for change_dns

--- cloudflare_utils.py
+++ cloudflare_utils.py
@@ -14,17 +14,21 @@ def change_dns(new_ip):
         'type': 'A',
     }
 
-    try:
-        response = requests.patch(
-            f"https://api.cloudflare.com/client/v4/zones/{main.CONFIG['cf_zone_id']}/dns_records/{main.CONFIG['cf_domain_id']}",
-            headers=headers,
-            json=json_data,
-            timeout=main.CONFIG['timeout'] * 4
-        )
-        response.raise_for_status()
-        if response.status_code == 200:
-            print('dns changed')
-        else:
+    for i in range(3):
+        try:
+            response = requests.patch(
+                f"https://api.cloudflare.com/client/v4/zones/{main.CONFIG['cf_zone_id']}/dns_records/{main.CONFIG['cf_domain_id']}",
+                headers=headers,
+                json=json_data,
+                timeout=main.CONFIG['timeout'] * 4
+            )
+            response.raise_for_status()
+            if response.status_code == 200:
+                break
+            else:
+                continue
+        except Exception as e:
             print('nooooo')
-    except Exception as e:
-        print('nooooo')
+    else:
+        raise Exception('nooooo')
+    print('yessss')
